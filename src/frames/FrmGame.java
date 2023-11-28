23e257423b313a1eedc4f119ab41654eb64c7a7b
BuildTools <unconfigured@null.spigotmc.org>
2023-11-28 10:12:40 -0800

    Se añadio pantalla completa funcional

--- src/frames/FrmGame.java
+++ src/frames/FrmGame.java
@@ -1,13 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package frames;
 
-/**
- *
- * @author alexc
- */
-public class FrmGame {
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.HeadlessException;
+import javax.swing.JFrame;
+
+
+public class FrmGame extends JFrame {
+
+    public int screenWidth = 900;
+    public int screenHeight = 600;
+    
+    public FrmGame() {
+        this.setUndecorated(true);
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+        this.setLocation(this.getLocation().x - screenWidth /2, this.getLocation().y - screenHeight/2);
+        this.setResizable(false);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        Dimension d = new Dimension(screenWidth, screenHeight);
+        this.setSize(d);
+        
+        
+        this.setExtendedState(MAXIMIZED_BOTH);
+        
+    }
+    
+    
     
 }
---
+++ src/main/Main.java
@@ -0,0 +1,12 @@
+
+package main;
+
+import frames.FrmGame;
+
+public class Main {
+
+    public static void main(String[] args) {
+        FrmGame game = new FrmGame();
+    }
+    
+}
